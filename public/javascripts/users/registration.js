b0114f60f8c2900cdff35e168cabac9461b107f2
Vijayshree Moyade <vijayshree.moyade@cuelogic.com>
2020-01-27 17:38:45 +0530

    modular structure added

---
+++ public/javascripts/users/registration.js
@@ -0,0 +1,65 @@
+var registration = (function(){
+	var signupForm = document.getElementById("registrationForm"), userData=[]
+	var profileIcon = document.getElementById("profile_image")
+	signupForm.onsubmit = function(event){
+		try
+		{
+			event.preventDefault();
+			event.stopPropagation();
+			usersItem = JSON.parse(localStorage.getItem("users")) || []
+			Array.from(event.target.elements).map(function(element){
+				{ userData.push({[element.name]: element.value}) }
+			})
+			usersItem.push(userData)
+			localStorage.setItem("users", JSON.stringify(usersItem))
+			alert("Signup successfully!")
+			location.replace('/public/src/users/login.html')
+		}
+		catch(e)
+		{
+			alert(e)
+			signupForm.reset();
+		}
+	}
+	
+	profileIcon.onchange = function(){
+		var reader = new FileReader();
+		reader.onload = function () {
+			var thisImage = reader.result;
+			document.getElementById("avatarImg").value = thisImage
+			document.getElementById("preview").src = thisImage
+		};
+		reader.readAsDataURL(this.files[0]);
+	}
+
+	(function(){
+		//"use strict"; 
+		var inputs = document.getElementsByClassName('validateSignup');
+		Array.prototype.forEach.call(inputs, (input) => {
+			input.addEventListener('keyup', (element) => {
+				try 
+				{
+					if(!element.target.checkValidity())
+					{
+						element.target.classList.remove('is-valid')
+						element.target.classList.add('is-invalid')
+						document.getElementsByClassName("btn-primary")[0].disabled= true
+					}
+					else
+					{
+						element.target.classList.remove('is-invalid')
+						element.target.classList.add('is-valid')
+						document.getElementsByClassName("btn-primary")[0].disabled= false
+					}
+				} 
+				catch (error) {
+					document.getElementsByClassName(element.target.name+"_feedback")[0].innerHTML = error;
+				}
+				finally
+				{
+					document.getElementsByClassName(element.target.name+"_feedback")[0].innerHTML = element.target.validationMessage;
+				}
+			})
+		});
+	})();
+})();
--- public/javascripts/workers/worker.js
+++ public/javascripts/workers/worker.js
@@ -1,8 +1,8 @@
-(function(){
+var Worker = (function(){
   var worker;
   if (typeof(Worker) !== "undefined") {
     if (typeof(worker) == "undefined") {
-      worker = new Worker('javascripts/background_worker.js');
+      worker = new Worker('/public/javascripts/workers/background_worker.js');
     }
     worker.onmessage = function(event) {
       if(event.data == 'logout')
@@ -10,7 +10,7 @@
         if(localStorage.loggedIn==undefined || !eval(localStorage.loggedIn)){
           if(window.location.href.match('dashboard').length > 0)
           {
-            window.location.replace('/public/index.html')
+            window.location.replace('/public/src/users/login.html')
           }
           history.pushState(null, null, location.href);
           window.onpopstate = function () {
